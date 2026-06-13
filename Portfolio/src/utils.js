5a228dccdc60737b80e42349d69a4f813f3275b3
Yorian Fransz <sydney-cook@outlook.com>
2026-06-13 13:54:18 +0200

    broken image en link utils fix

--- Portfolio/src/utils.js
+++ Portfolio/src/utils.js
@@ -1,3 +1,10 @@
 export const getImageUrl = (path) => {
-  return new URL(`../assets/${path}`, import.meta.url).href;
+  console.log("IMAGE PATH:", path);
+
+  if (!path) {
+    console.error("IMAGE PATH IS UNDEFINED");
+    return "";
+  }
+
+  return new URL(`/assets/${path}`, import.meta.url).href;
 };
